a8dae4a343639d3536c0a89608037232d2550f3c
Jaron Kennel <jaronkk@gmail.com>
2014-04-11 13:05:01 -0400

    Fixed demo page href and src attributes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Discover::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'records#show'
 
+  get 'demo' => 'demo#catcher', as: :demo_root
+  get 'demo/:action' => 'demo', as: :demo
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
