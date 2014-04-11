ed8c5f3f7a0036e1dc31a5cbc3be287211b7ac09
Jaron Kennel <jaronkk@gmail.com>
2014-04-11 13:21:40 -0400

    Renamed tab class to prevent various scripts from running

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,13 @@
 class RecordsController < ApplicationController
   def show
-
+    respond_to do |format|
+      format.html do
+        if request.xhr?
+          render layout: false
+        else
+          render
+        end
+      end
+    end
   end
 end
