f46701a6421035cd8bd271a6931393c546f6adb9
Jaron Kennel <jaronkk@gmail.com>
2014-04-15 15:15:10 -0400

    Adding display field examples

--- app/models/primo_display_field.rb
+++ app/models/primo_display_field.rb
@@ -1,6 +1,8 @@
 class PrimoDisplayField < ActiveRecord::Base
   validates :key, presence: true, uniqueness: true
 
+  has_many :examples, class_name: "PrimoDisplayFieldExample", primary_key: :key, foreign_key: :key
+
   def self.log_unknown(key)
     key = key.to_s
     display_field = self.find_by_key(key)
@@ -10,5 +12,14 @@ class PrimoDisplayField < ActiveRecord::Base
     end
     yield display_field if block_given?
     display_field
+  end
+
+  def add_example(record_id, body)
+    example = examples.where(record_id: record_id).first
+    if example.nil?
+      example = examples.build(record_id: record_id, body: body)
+      example.save
+    end
+    example
   end
 end
--- spec/models/primo_display_field_spec.rb
+++ spec/models/primo_display_field_spec.rb
@@ -1,19 +1,33 @@
 require 'spec_helper'
 
 describe PrimoDisplayField do
+  let(:key) {'test_key'}
+
   describe '#key' do
     it 'is required' do
       expect(subject).to have(1).error_on(:key)
-      subject.key = 'test'
+      subject.key = key
       expect(subject).to have(0).errors_on(:key)
     end
 
     it 'is unique' do
-      described_class.create(key: 'test')
-      subject.key = 'test'
+      described_class.create(key: key)
+      subject.key = key
       expect(subject).to have(1).error_on(:key)
-      subject.key = 'test2'
+      subject.key = "another_#{key}"
       expect(subject).to have(0).errors_on(:key)
+    end
+  end
+
+  describe 'saved' do
+    let(:record_id) { 'ndu_aleph000188916' }
+    let(:body) { 'Test Body' }
+    subject { described_class.create(key: key) }
+
+    describe '#add_example' do
+      it 'adds an example' do
+        expect{subject.add_example(record_id, body)}.to change{subject.examples.count}.by(1)
+      end
     end
   end
 
@@ -22,19 +36,19 @@ describe PrimoDisplayField do
 
     describe '#log_unknown' do
       it 'creates a record' do
-        expect{subject.log_unknown('test')}.to change{subject.count}.by(1)
+        expect{subject.log_unknown(key)}.to change{subject.count}.by(1)
       end
 
       it 'returns an existing record' do
-        record = subject.create(key: 'test')
-        expect{subject.log_unknown('test')}.to change{subject.count}.by(0)
-        expect(subject.log_unknown('test')).to eq(record)
+        record = subject.create(key: key)
+        expect{subject.log_unknown(key)}.to change{subject.count}.by(0)
+        expect(subject.log_unknown(key)).to eq(record)
       end
 
       it 'receives a block' do
         expect_any_instance_of(subject).to receive(:block_test)
-        record = subject.create(key: 'test')
-        subject.log_unknown('test') do |display_field|
+        record = subject.create(key: key)
+        subject.log_unknown(key) do |display_field|
           display_field.block_test
           expect(record).to be == display_field
         end
