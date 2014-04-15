5e200d685c396db8a861a0783e60b1c73fa2728f
Jaron Kennel <jaronkk@gmail.com>
2014-04-15 13:24:28 -0400

    Detail content

--- app/decorators/record_decorator.rb
+++ app/decorators/record_decorator.rb
@@ -5,11 +5,40 @@ class RecordDecorator < Draper::Decorator
     self.new(record)
   end
 
-  def display_fields
-    object.display_fields
+  def detail_content
+    if @detail_content.nil?
+      @detail_content = []
+      detail_methods.each do |method, label|
+        method_content = self.send(method)
+        if method_content.present?
+          @detail_content << [label, method_content]
+        end
+      end
+    end
+    @detail_content
+  end
+
+  def detail_methods
+    {
+      title: "Title",
+      author: "Author",
+      published: "Published"
+    }
+  end
+
+  def title
+    object.title
   end
 
   def author
+    object.creator_contributor
+  end
 
+  def published
+    object.publisher_provider
+  end
+
+  def display_fields
+    object.display_fields
   end
 end
--- spec/decorators/record_decorator_spec.rb
+++ spec/decorators/record_decorator_spec.rb
@@ -1,23 +1,75 @@
 require 'spec_helper'
 
 describe RecordDecorator do
-  let(:search_id) { "ndu_aleph000188916" }
+  subject { described_class.new(nil) }
 
-  subject do
-    VCR.use_cassette 'discovery_query/find_by_id' do
-      described_class.find(search_id)
+  describe '#detail_content' do
+    before do
+      subject.stub(:detail_methods).and_return({title: 'Title', author: 'Author'})
+    end
+
+    it 'returns an array of arrays' do
+      subject.stub(:title).and_return('Test Title')
+      subject.stub(:author).and_return('John Doe')
+
+      expect(subject.detail_content).to be == [['Title', 'Test Title'],['Author', 'John Doe']]
+    end
+
+    it 'only returns values for methods with content' do
+      subject.stub(:title).and_return('Test Title')
+      subject.stub(:author).and_return(nil)
+
+      expect(subject.detail_content).to be == [['Title', 'Test Title']]
     end
   end
 
-  describe '#object' do
-    it 'is a DiscoveryRecord' do
-      expect(subject.object).to be_a_kind_of(DiscoveryRecord)
+  describe '#detail_methods' do
+      it 'responds to each method' do
+        subject.detail_methods.each do |method,label|
+          expect(subject).to respond_to(method)
+        end
+      end
     end
-  end
 
-  describe '#display_fields' do
-    it 'is a hash' do
-      expect(subject.display_fields).to be_a_kind_of(Hash)
+  describe 'found record' do
+    let(:search_id) { "ndu_aleph000188916" }
+
+    subject do
+      VCR.use_cassette 'discovery_query/find_by_id' do
+        described_class.find(search_id)
+      end
+    end
+
+    let(:object) { subject.object }
+
+    describe '#object' do
+      it 'is a DiscoveryRecord' do
+        expect(subject.object).to be_a_kind_of(DiscoveryRecord)
+      end
+    end
+
+    describe '#display_fields' do
+      it 'is a hash' do
+        expect(subject.display_fields).to be_a_kind_of(Hash)
+      end
+    end
+
+    describe '#title' do
+      it 'is the object title' do
+        expect(subject.title).to be == object.title
+      end
+    end
+
+    describe '#author' do
+      it 'is the object author' do
+        expect(subject.author).to be == object.creator_contributor
+      end
+    end
+
+    describe '#published' do
+      it 'is the object publisher_provider' do
+        expect(subject.published).to be == object.publisher_provider
+      end
     end
   end
 end
