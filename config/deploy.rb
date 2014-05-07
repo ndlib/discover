2b0619a2ed083163ae12b903f122b53988d31a33
Jaron Kennel <jaronkk@gmail.com>
2014-05-07 11:51:15 -0400

    Set up hipchat and prod deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,29 +7,26 @@ require 'hesburgh/rails'
 require 'hesburgh/rails_db'
 require 'hesburgh/jenkins'
 # require 'hesburgh/whenever'
+require 'hipchat/capistrano'
 
 set :application, 'discover'
 set :repository,  "git@git.library.nd.edu:discover"
 # set :application_symlinks, ['config/initializers/devise_secret.rb']
+
+set :hipchat_token, "c290a842542721d6aee18a3cb900a1"
+set :hipchat_room_name, "Web and Software Engineering"
+set :hipchat_announce, false # notify users?
 
 desc "Setup for the Pre-Production environment"
 task :pre_production do
   # Customize pre_production configuration
   set :rails_env, 'pre_production'
   role :app, "discoverpprd-vm.library.nd.edu"
-
-  set :hipchat_token, "c290a842542721d6aee18a3cb900a1"
-  set :hipchat_room_name, "Web and Software Engineering"
-  set :hipchat_announce, false # notify users?
 end
 
 desc "Setup for the production environment"
 task :production do
   # Customize production configuration
   set :rails_env, 'production'
-  role :app, "discoverpprd-vm.library.nd.edu"
-
-  set :hipchat_token, "c290a842542721d6aee18a3cb900a1"
-  set :hipchat_room_name, "Web and Software Engineering"
-  set :hipchat_announce, false # notify users?
+  role :app, "discoverprod-vm.library.nd.edu"
 end
