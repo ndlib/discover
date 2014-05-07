a4989b31e7a83831591c88dde5741d609aebe5e5
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-05-07 10:26:58 -0400

    fix the test.

--- spec/service/primo_field_splitter_spec.rb
+++ spec/service/primo_field_splitter_spec.rb
@@ -5,27 +5,27 @@ describe PrimoFieldSplitter do
 
   it "splits on the -- " do
     test = 'part1--part2'
-    expect(PrimoFieldSplitter.call(test)).to eq(['part1', 'part2'])
+    expect(PrimoFieldSplitter.dash(test)).to eq(['part1', 'part2'])
   end
 
   it "trims the results of each restults" do
     test = '  part1  --  part2  '
-    expect(PrimoFieldSplitter.call(test)).to eq(['part1', 'part2'])
+    expect(PrimoFieldSplitter.dash(test)).to eq(['part1', 'part2'])
   end
 
   it "returns only the one result when there is no delimiter" do
     test = 'part1  part2'
-    expect(PrimoFieldSplitter.call(test)).to eq([test])
+    expect(PrimoFieldSplitter.dash(test)).to eq([test])
   end
 
   it "returns nil when the nil is passed in" do
     test = nil
-    expect(PrimoFieldSplitter.call(test)).to be_nil
+    expect(PrimoFieldSplitter.dash(test)).to be_nil
   end
 
 
   it "returns empty string if the data is empty" do
     test = ''
-    expect(PrimoFieldSplitter.call(test)).to eq('')
+    expect(PrimoFieldSplitter.dash(test)).to eq('')
   end
 end
