9f3df375c5139bdbc6c3a4259e364595545234ee
Jaron Kennel <jaronkk@gmail.com>
2014-05-13 13:16:57 -0400

    More methods for aleph catalog linking

--- app/service/aleph_catalog_link.rb
+++ app/service/aleph_catalog_link.rb
@@ -3,15 +3,23 @@ class AlephCatalogLink < Draper::Decorator
     object
   end
 
+  def institution_code
+    id.gsub(/_.*/,'')
+  end
+
   def system_number
     object.gsub(/[^\d]+/,'')
   end
 
+  def local_base
+    "#{institution_code}01pub"
+  end
+
   def direct_path
-    "/F/?func=direct&doc_number=#{system_number}"
+    "/F/?func=direct&doc_number=#{system_number}&local_base=#{local_base}"
   end
 
   def search_path
-    "/F/?func=scan&scan_code=SYS&scan_start=#{system_number}"
+    "/F/?func=scan&scan_code=SYS&scan_start=#{system_number}&local_base=#{local_base}"
   end
 end
--- spec/service/aleph_catalog_link_spec.rb
+++ spec/service/aleph_catalog_link_spec.rb
@@ -2,23 +2,36 @@ require 'spec_helper'
 
 describe AlephCatalogLink do
   let(:record_id) { "ndu_aleph001890313" }
+  let(:system_number) { record_id.gsub(/[^\d]+/,'')}
   subject { described_class.new(record_id) }
 
   describe '#system_number' do
     it 'is the numeric system number' do
-      expect(subject.system_number).to eq("001890313")
+      expect(subject.system_number).to eq(system_number)
     end
   end
 
   describe '#direct_path' do
     it 'is the direct link path' do
-      expect(subject.direct_path).to eq("/F/?func=direct&doc_number=001890313")
+      expect(subject.direct_path).to eq("/F/?func=direct&doc_number=#{system_number}&local_base=ndu01pub")
     end
   end
 
   describe '#search_path' do
     it 'is the search path on aleph' do
-      expect(subject.search_path).to eq("/F/?func=scan&scan_code=SYS&scan_start=001890313")
+      expect(subject.search_path).to eq("/F/?func=scan&scan_code=SYS&scan_start=#{system_number}&local_base=ndu01pub")
+    end
+  end
+
+  describe '#institution_code' do
+    it 'is ndu' do
+      expect(subject.institution_code).to eq('ndu')
+    end
+  end
+
+  describe '#local_base' do
+    it 'is ndu01pub' do
+      expect(subject.local_base).to eq('ndu01pub')
     end
   end
 end
