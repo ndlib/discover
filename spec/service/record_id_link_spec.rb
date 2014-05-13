0a69f9f786d3868061e1ba44a67e9bb1d3cafe82
Jaron Kennel <jaronkk@gmail.com>
2014-05-13 15:30:36 -0400

    Record id linker class

--- spec/service/record_id_link_spec.rb
+++ spec/service/record_id_link_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
 
 describe RecordIdLink do
-  subject { described_class.new(nil) }
+  let(:record_id) { 'ndu_aleph12345' }
+  subject { described_class.new(record_id) }
 
-  describe '#render_class' do
-    it 'renders for Aleph' do
-      subject.stub(:object).and_return 'ndu_aleph12345'
+  describe '#render' do
+    let(:render_class) { double() }
+
+    it 'renders with render_class' do
+      expect(render_class).to receive(:render)
+      expect(render_class).to receive(:present?).and_return(true)
+      subject.stub(:render_class).and_return(render_class)
+      subject.render
+    end
+
+    it 'renders the record_id if there is no render_class' do
+      expect(render_class).to receive(:present?).and_return(false)
+      subject.stub(:render_class).and_return(render_class)
+      expect(subject.render).to eq(record_id)
+    end
+  end
+
+  describe 'Aleph' do
+    let(:record_id) { 'ndu_aleph12345' }
+    it '#render_class returns RecordIdLinkAleph' do
       expect(subject.render_class).to eq(RecordIdLinkAleph)
     end
   end
