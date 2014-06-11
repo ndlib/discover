f75f0dc7062ea3636c2fe7cbdb5c34c9b09240bb
Jaron Kennel <jaronkk@gmail.com>
2014-06-11 16:17:22 -0400

    Adding support for new fullview request method

--- app/query/discovery_query.rb
+++ app/query/discovery_query.rb
@@ -4,9 +4,16 @@ class DiscoveryQuery
     self.new.find_by_id(id)
   end
 
+  def self.fullview(id)
+    self.new.fullview(id)
+  end
 
   def find_by_id(id)
     DiscoveryRecord.new(HesburghAPI2::Discovery.record(id))
+  end
+
+  def fullview(id)
+    DiscoveryRecord.new(HesburghAPI2::Discovery.fullview(id))
   end
 
 end
