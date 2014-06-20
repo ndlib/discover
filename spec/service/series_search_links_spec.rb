24446ebf635597f2fc4b5a5bfb8084a1930ab8d4
Jaron Kennel <jaronkk@gmail.com>
2014-06-20 10:35:07 -0400

    Switching back to primo-fe1 for now

--- spec/service/series_search_links_spec.rb
+++ spec/service/series_search_links_spec.rb
@@ -9,7 +9,7 @@ describe SeriesSearchLinks do
   end
 
   it "returns links if there are series passed in " do
-    expect(SeriesSearchLinks.render([{"series_title"=>"Rowling, J. K. Harry Potter series", "series_volume"=>"3."}], :series)).to eq("<ul class=\"ndl-series-search\"><li class=\"ndl-series-search-1\"><a href=\"http://primo-fe2.library.nd.edu:1701/primo_library/libweb/action/search.do?fn=search&amp;mode=Advanced&amp;tab=onesearch&amp;vid=NDU&amp;vl%2816833817UI0%29=lsr30&amp;vl%281UIStartWith0%29=exact&amp;vl%28freeText0%29=Rowling%2C+J.+K.+Harry+Potter+series\" title=\"Search for &quot;Rowling, J. K. Harry Potter series&quot;\">Rowling, J. K. Harry Potter series</a>; <span>3.</span></li></ul>")
+    expect(SeriesSearchLinks.render([{"series_title"=>"Rowling, J. K. Harry Potter series", "series_volume"=>"3."}], :series)).to eq("<ul class=\"ndl-series-search\"><li class=\"ndl-series-search-1\"><a href=\"http://primo-fe1.library.nd.edu:1701/primo_library/libweb/action/search.do?fn=search&amp;mode=Advanced&amp;tab=onesearch&amp;vid=NDU&amp;vl%2816833817UI0%29=lsr30&amp;vl%281UIStartWith0%29=exact&amp;vl%28freeText0%29=Rowling%2C+J.+K.+Harry+Potter+series\" title=\"Search for &quot;Rowling, J. K. Harry Potter series&quot;\">Rowling, J. K. Harry Potter series</a>; <span>3.</span></li></ul>")
   end
 
 end
