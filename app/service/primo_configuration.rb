a181fbf4fbae06fb0b78d03ecb268a50e480cdc5
Jaron Kennel <jaronkk@gmail.com>
2014-06-26 14:50:57 -0400

    Primo configuration

--- app/service/primo_configuration.rb
+++ app/service/primo_configuration.rb
@@ -2,22 +2,27 @@ class PrimoConfiguration
   attr_reader :vid
 
   def initialize(vid)
-    @vid = vid.to_s
+    @vid = vid.to_s.upcase
   end
 
-  def vid_configuration
-    vids[vid] || {}
+  def tabs
+    vid_configuration['tabs']
   end
 
-  def vids
-    configuration['vids'] || {}
-  end
+  private
+    def vid_configuration
+      vids[vid] || {}
+    end
 
-  def configuration
-    @configuration ||= self.class.load_configuration
-  end
+    def vids
+      configuration['vids'] || {}
+    end
 
-  def self.load_configuration
-    YAML.load_file(Rails.root.join('config', 'primo.yml'))
-  end
+    def configuration
+      @configuration ||= self.class.load_configuration
+    end
+
+    def self.load_configuration
+      YAML.load_file(Rails.root.join('config', 'primo.yml'))
+    end
 end
--- spec/service/primo_configuration_spec.rb
+++ spec/service/primo_configuration_spec.rb
@@ -4,9 +4,22 @@ describe PrimoConfiguration do
   let(:vid) { 'NDU' }
   subject { described_class.new(vid)}
 
+  describe '#vid' do
+    it 'is the vid' do
+      expect(subject.vid).to eq('NDU')
+    end
+  end
+
   describe '#vid_configuration' do
     it 'loads the vid config' do
-      expect(subject.vid_configuration).to be_a_kind_of(Hash)
+      expect(subject.send(:vid_configuration)).to be_a_kind_of(Hash)
+    end
+  end
+
+  describe '#tabs' do
+    it 'is an array' do
+      expect(subject.tabs).to be_a_kind_of(Array)
+      expect(subject.tabs.first).to eq('onesearch')
     end
   end
 end
