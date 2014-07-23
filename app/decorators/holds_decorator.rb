cca4edfaa53d17737b35e2cc19c8e9f976fc0fa7
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-07-23 07:47:42 -0400

    fix the saving mechanism to not use the session to save form choices but to use get params.

--- app/decorators/holds_decorator.rb
+++ app/decorators/holds_decorator.rb
@@ -4,42 +4,9 @@ class HoldsDecorator < Draper::Decorator
     object.title.first
   end
 
-  def page_title
-    title
-  end
-
 
   def volumes
 
   end
-
-end
-
-
-class HoldsVolume
-
-  def initialize(volume, items)
-    @volume = volume
-    @items = items
-  end
-
-  def id
-    @volume[:enumeration]
-  end
-
-
-  def title
-    @volume[:description]
-  end
-
-
-  def items
-
-  end
-end
-
-
-class HoldsItem
-
 
 end
---
+++ app/decorators/holds_tab.rb
@@ -0,0 +1,38 @@
+class HoldsTab < PrimoRecordTab
+
+  def url_params(merge_params = {})
+    {
+      id: id,
+      vid: vid,
+      persisted_params: holds_request.params
+    }.merge(merge_params)
+  end
+
+
+  def save_input(new_params)
+    holds_request.save_params(new_params)
+  end
+
+
+  def persisted_params
+    params[:persisted_params] || {}
+  end
+
+
+
+  def page_title
+    record.title.first
+  end
+
+
+  private
+
+    def holds_request
+      # create a new object passing the previous data
+      @holds_request ||= HoldsRequest.new(persisted_params)
+    end
+
+
+end
+
+
