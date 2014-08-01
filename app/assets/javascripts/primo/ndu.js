2e56edf3ccaa30f69a180b85f21759d7529ed4ea
Jaron Kennel <jaronkk@gmail.com>
2014-08-01 13:05:21 -0400

    Ran javascript through http://jsbeautifier.org/

--- app/assets/javascripts/primo/ndu.js
+++ app/assets/javascripts/primo/ndu.js
@@ -1,11 +1,10 @@
-
-$(document).ready(function(){
+$(document).ready(function() {
 
   var fd = EXLTA_isFullDisplay();
 
   $('.EXLResultTabs').each(
-    function(){
-      if(!$(this).children('.EXLLocationsTab').attr('class') && !$(this).children('.EXLViewOnlineTab').attr('class')){
+    function() {
+      if (!$(this).children('.EXLLocationsTab').attr('class') && !$(this).children('.EXLViewOnlineTab').attr('class')) {
         $(this).parent().parent().parent().children('.EXLSummaryContainer').children('.EXLSummaryFields').children('.EXLResultAvailability').html('');
 
       }
@@ -13,27 +12,39 @@ $(document).ready(function(){
   );
 
   $('.EXLReviewsTab').each(
-    function(){
+    function() {
       var rt = $(this);
       var dn = EXLTA_recordId($(this));
-      if(dn){
+      if (dn) {
         var ddud = 'pnxId=' + dn;
         var ddui = '/primo_library/libweb/tiles/local/docdel.jsp';
-        $.ajax({type: "get", url: ddui, dataType: "html", data: ddud,  success: function(data){
-          var dre = /http/;
-          if(data.match(dre)){
-            rt.after('<li id="docDelUrl" class="EXLResultTab">' + data + '</li>');
+        $.ajax({
+          type: "get",
+          url: ddui,
+          dataType: "html",
+          data: ddud,
+          success: function(data) {
+            var dre = /http/;
+            if (data.match(dre)) {
+              rt.after('<li id="docDelUrl" class="EXLResultTab">' + data + '</li>');
+            }
           }
-        }});
+        });
         var rud = 'pnxId=' + dn + '&institution=NDU';
         var rui = '/primo_library/libweb/tiles/local/request.jsp';
-        $.ajax({type: "get", url: rui, dataType: "html", data: rud,  success: function(data){
-          var dre = /<div id="requestable">yes<\/div>/;
-          if(data.match(dre)){
-            rt.siblings('.EXLRequestTab').show().css('list-style-type', 'none');
-          }
+        $.ajax({
+          type: "get",
+          url: rui,
+          dataType: "html",
+          data: rud,
+          success: function(data) {
+            var dre = /<div id="requestable">yes<\/div>/;
+            if (data.match(dre)) {
+              rt.siblings('.EXLRequestTab').show().css('list-style-type', 'none');
+            }
 
-        }});
+          }
+        });
 
 
       }
@@ -41,11 +52,11 @@ $(document).ready(function(){
 
     });
 
-  
+
 
 });
 
 
-function EXLTA_recordId(element){
+function EXLTA_recordId(element) {
   return $(element).parents('.EXLResult').find('.EXLResultRecordId').attr('id');
 }
