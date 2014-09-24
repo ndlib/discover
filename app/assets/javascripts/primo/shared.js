385b36fceeab7d1e7e11b58baecf3daac25307d4
Jaron Kennel <jaronkk@gmail.com>
2014-09-24 10:58:50 -0400

    Created a shared javascript include file to make sure we're not missing functionality

---
+++ app/assets/javascripts/primo/shared.js
@@ -0,0 +1,16 @@
+//
+//= require ./help.js
+//= require ./jquery.colorbox-min.js
+//= require ./jquery.simpledialog.0.1.min.js
+//= require ./jquery.tablesorter.js
+//= require ./jquery.highlight.js
+//= require ./malc.js
+//= require ./modal-popups.js
+//= require ./wc.js
+//= require ./fix-dateslider.js
+//= require ./fix-facets.js
+//= require ./fix-browse-search.js
+//= require ./fix-eshelf-class.js
+//= require ./export_links.js
+//= require ./discover-tabs.js
+//= require ./discover-request-tab.js
