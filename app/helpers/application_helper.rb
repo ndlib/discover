74d616d3317b24c115752df8d3d23e12165f3dba
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-10-07 12:50:46 -0400

    update map ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def ul_list(title, list, css_class = "")
     return "" if list.empty? || list.join.empty?
 
-    ret = content_tag(:h5, title)
+    ret = content_tag(:div, title)
     ret += content_tag(:ul, class: css_class) do
       list.each { |note| concat(content_tag(:li, note)) }
     end
